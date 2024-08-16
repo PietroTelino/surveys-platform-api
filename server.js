572ef31f95af6baada264c0ee840bb22039b2096
Pietro <pietrotelino@hotmail.com>
2024-08-15 22:06:59 -0300

    Permitindo requisição de qualquer ip

--- server.js
+++ server.js
@@ -10,19 +10,20 @@ const allowedOrigins = [
 const app = express();
 const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
 
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin) return callback(null, true);
+// app.use(
+//     cors({
+//         origin: function (origin, callback) {
+//             if (!origin) return callback(null, true);
 
-            if (allowedOrigins.indexOf(origin) === -1) {
-                return callback(new Error(msg), false);
-            }
+//             if (allowedOrigins.indexOf(origin) === -1) {
+//                 return callback(new Error(msg), false);
+//             }
 
-            return callback(null, true);
-        }
-    })
-);
+//             return callback(null, true);
+//         }
+//     })
+// );
+app.use(cors());
 app.use(express.json());
 app.use(userRoutes);
 app.use(surveyRoutes);
